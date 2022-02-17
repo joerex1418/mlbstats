8b526302c7598166d149678eeaf04c4b616a44f6
Joe <joe.rechenmacher@gmail.com>
2022-02-17 12:02:20 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     long_description=long_description,
     url='https://github.com/joerex1418/simplestats-mlb',
     project_urls = {
-        "Issues": "https://github.com/joerex1418/simplestats/wiki/MLB",
+        "Issues": "https://github.com/joerex1418/simplestats/wiki",
     },
     license='GPU',
     packages=setuptools.find_packages(where='/simplestats-mlb/',include=["mlb"]),
